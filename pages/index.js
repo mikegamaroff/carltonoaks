df2e2e9553230a54c31be575acccf5351556447a
Mike Gamaroff <mke@gamaroff.com>
2020-09-09 14:29:52 -0500

    Paralax in place

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,188 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { Component } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Parallax from "parallax-js";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    topslide: 0,
+    bottomslide: 1,
+    currentlyLoading: null,
+    key: "(EN]KP}pzz]avzqE96XnW?AtuZju9",
+    validated: false,
+  };
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  componentDidMount() {
+    var scene = document.getElementById("scene");
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    var parallaxInstance = new Parallax(scene, {
+      relativeInput: true,
+    });
+  }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <main className={styles.main}>
+          <div className="roomContainer">
+            <div className="sceneContainer">
+              <div>
+                <div
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    margin: "auto",
+                  }}
+                  id="scene"
+                  data-relative-input="true"
+                  data-limit-x="false"
+                  /*    data-limit-y="0" */
+                  data-scalar-x="20"
+                  data-scalar-y="5"
+                  data-friction-x=".1"
+                  data-origin-x=".5"
+                >
+                  {/*     <div style={{ marginTop: "0px" }} data-depth="0">
+                <img
+                  src="images/background.jpg"
+                  style={{
+                    width: "100%",
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                  }}
+                />
+              </div> */}
+                  <div data-depth="0.5" style={{}}>
+                    <img
+                      src="images/empty.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(676%)",
+                      }}
+                    />
+                  </div>
+                  <div data-depth="0.4" style={{}}>
+                    <img
+                      src="images/floor_01.jpg"
+                      style={{
+                        width: "calc(100% + 20px)",
+                        transform: "translate(-20px,664%)",
+                      }}
+                    />
+                  </div>
+                  <div data-depth="0.5" style={{}}>
+                    <img
+                      src="images/floor_01b.jpg"
+                      style={{
+                        width: "calc(100% + 20px)",
+                        transform: "translate(-20px,384%)",
+                      }}
+                    />
+                  </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+                  <div style={{ marginTop: "0px" }} data-depth="0.1">
+                    <img
+                      src="images/golfClub.jpg"
+                      style={{
+                        width: "40%",
+
+                        transform: "translate(75%, 35%)",
+                      }}
+                    />
+                  </div>
+                  <div style={{ marginTop: "0px" }} data-depth="0.4">
+                    <img
+                      src="images/floor_02.jpg"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(718%)",
+                      }}
+                    />
+                  </div>
+                  <div style={{ marginTop: "0px" }} data-depth="0.1">
+                    <img
+                      src="images/ceiling.jpg"
+                      style={{
+                        width: "100%",
+
+                        transform: "translateY(0%)",
+                      }}
+                    />
+                  </div>
+                  <div style={{ marginTop: "0px" }} data-depth="0.3">
+                    <img
+                      src="images/floor_03.jpg"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(1248%)",
+                      }}
+                    />
+                  </div>
+
+                  <div style={{ marginTop: "0px" }} data-depth="0.2">
+                    <img
+                      src="images/backWall.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(75%)",
+                      }}
+                    />
+                  </div>
+                  <div style={{ marginTop: "0px" }} data-depth="0.3">
+                    <img
+                      src="images/middle.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(50%)",
+                      }}
+                    />
+                  </div>
+
+                  <div style={{ marginTop: "0px" }} data-depth="0.4">
+                    <img
+                      src="images/front.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(40%)",
+                      }}
+                    />
+                  </div>
+                  <div style={{ marginTop: "0px" }} data-depth="0.7">
+                    <img
+                      src="images/lamp.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(40%);",
+                      }}
+                    />
+                  </div>
+                  <div data-depth="0.5" style={{}}>
+                    <img
+                      src="images/empty.png"
+                      style={{
+                        width: "100%",
+                        transform: "translateY(676%)",
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+export default Home;
